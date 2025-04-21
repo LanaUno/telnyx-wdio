0922d7c79149948152aaf5692690a228b9cf8845
Svitlana Andrushenko <wwwli4e@gmail.com>
2025-04-21 19:01:26 +0300

    change config

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,7 @@ exports.config = {
     capabilities: [
         {
         browserName: 'chrome',
-        browserVersion: '124.0.6367.9',
+        browserVersion: '130.0.6723.116',
         'goog: chromeOptions' : {
             args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
         }
